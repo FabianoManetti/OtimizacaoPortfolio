176acb734f47b7e88cba0b04fbfe29e15704fae9
Fabiano Manetti <102125988+FabianoManetti@users.noreply.github.com>
2022-07-06 08:07:24 -0300

    Update Pagina_3.py

--- pages/Pagina_3.py
+++ pages/Pagina_3.py
@@ -8,7 +8,6 @@ cadastro_fii = pd.read_csv('fundosListados.csv', sep = ',', encoding = 'utf-8')
 cadastro_etf = pd.read_csv('etf_bdr.csv', sep = ';', encoding = 'latin-1')
 cadastro_stocks = pd.read_csv('cadastro_stocks.csv')
 
-lista_port_final
 st.info(st.session_state.port_final)
 st.info(portfolio_pesos_final)
 
